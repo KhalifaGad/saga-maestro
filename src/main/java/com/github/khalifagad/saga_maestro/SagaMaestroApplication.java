7360dba8408030bcd1c5620682a1f04a0a775b5f
KhalifaGad <kkhalifa.gad@gmail.com>
2024-12-25 10:45:15 +0200

    feat: Init

---
+++ src/main/java/com/github/khalifagad/saga_maestro/SagaMaestroApplication.java
@@ -0,0 +1,13 @@
+package com.github.khalifagad.saga_maestro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagaMaestroApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SagaMaestroApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/khalifagad/saga_maestro/SagaMaestroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.khalifagad.saga_maestro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaMaestroApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
